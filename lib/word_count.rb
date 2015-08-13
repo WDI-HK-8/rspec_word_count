f090a2898bcd11495f837a90a611c077dd0a1fb9
raptattention <klaw1794@gmail.com>
2015-08-13 10:18:33 +0800

    W7D4 Assessment

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,7 +1,30 @@
 class String
   def unique_word_count
+  	word_count = 0
+  	words = self.downcase.gsub(/[^a-z0-9\s]/i, '').split
+	for i in 0..words.length-1
+	 	unique = true
+	 	for j in i+1..words.length-1
+	 	    if words[i]==words[j]
+	 	        unique = false
+	    	end
+	    end
+	     word_count += 1 if unique==true
+	 end
+	 word_count
   end
   
   def word_frequency_count
-  end  
+    words = self.downcase.gsub(/[^a-z0-9\s]/i, '').split
+    wordHash = Hash.new
+    while (words.length > 0) do
+        frequency = 1
+        for i in 1..words.length-1
+            frequency+=1 if words[0]==words[i]
+        end
+        wordHash[words[0].to_s]=frequency
+        words.delete(words[0])
+    end
+    wordHash
+  end 
 end
