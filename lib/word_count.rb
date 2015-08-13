e40184bd13e4e78b7584542b09f3d326d330b120
Yohan <yohanology@gmail.com>
2015-08-13 10:19:07 +0800

    yohan solution

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,5 +1,17 @@
 class String
   def unique_word_count
+    count = 0 
+    if self == ""
+      0
+    else self.each_char do |char|
+        if char != /[^[:alnum:]]/
+          count += 1
+        else
+          count = 1
+        end
+      end
+      count
+    end
   end
   
   def word_frequency_count
