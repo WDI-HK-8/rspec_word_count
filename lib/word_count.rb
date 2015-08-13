860de3d7cc3360a04281ef1aacfa11d0d0ee6094
Carson Trent Long <CarsonLong@IOS-Syncy.local>
2015-08-13 10:36:21 +0800

    once I learned about .uniq I was able to do this

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,16 +1,11 @@
 class String
   def unique_word_count
     return 0 if self.length == 0
-    return self.gsub(/[^a-z ]/, '').scan(/\w+/).length
+    return self.gsub(/[^a-z ]/, '').scan(/\w+/).uniq.length
   end
   
   def word_frequency_count
-    return 0 if self.length == 0
-    array = self.scan(/\w+/)
-    init = array[0]
-    count = 1
-    array.length.times {
-      
-    }
+    return 0 if self.scan(/\w+/).length == 0
+    return self.gsub(/[^a-z ]/, '').scan(/\w+/).length - self.gsub(/[^a-z ]/, '').scan(/\w+/).uniq.length + 1
   end  
 end
