55fbcc23570a0841d135b432fb8ad49cb0ef41bd
raphaeldieu <dieuraphael@gmail.com>
2015-08-13 10:19:03 +0800

    w7d4

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,7 +1,22 @@
 class String
   def unique_word_count
+    count = 0
+    array = self.split(' ')
+    array.sort
+    array.each do |word|
+      if (word =! array[(word.indexOf) - 1])
+        count += 1
+      end
+    end
+  end
+      
+
   end
   
   def word_frequency_count
+    hash = Hash.new(0)
+    array = self.split(' ')
+    array.each {|word| hash[word] += 1}
+    return hash
   end  
 end
