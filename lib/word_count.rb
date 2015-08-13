5bc2c2ab79394aaced64d2ae37b1e1b268fad004
teelu <terrence.lun@gmail.com>
2015-08-13 10:05:53 +0800

    feat(assessment) - Morning Assessment W7D4

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,7 +1,20 @@
 class String
   def unique_word_count
+    #split scan for spaces
+    string_split = split(" ")
+    return string_split.length
   end
   
   def word_frequency_count
-  end  
+    return {} if length == 0
+    string_split = split(" ")
+    string_hash = Hash.new(0)
+    string_split.each do |string|
+      string_hash[string] += 1
+    end
+    string_hash.each {|word, value| 
+      return {word => value} if value > 1
+    }
+    return 1
+  end
 end
