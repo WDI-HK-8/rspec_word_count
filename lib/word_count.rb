d5385787812d473fda473332afb2fda77215da39
heiheychan <Bill_Chan@Chaks-MacBook-Air.local>
2015-08-13 10:21:00 +0800

    Still push it without any clue

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,7 +1,17 @@
 class String
   def unique_word_count
+    Self.uniq.count
   end
   
   def word_frequency_count
+    words = Self.split(" ")
+    frequency = Hash.new(0)
+
+    words.each do |word|
+      frequency[word] += 1
+    end
+
+    print frequency
+
   end  
 end
