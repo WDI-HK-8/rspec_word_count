c4a100103e4d239e3634e65e039a9fb79856977c
Richie Bostock <richiebostock@Richies-MacBook-Air.local>
2015-08-13 10:18:58 +0800

    richies w7d4 solution

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,7 +1,17 @@
 class String
   def unique_word_count
+    self.split.uniq.count
   end
   
   def word_frequency_count
+    hash = Hash.new(0)
+
+    self.split.each do |x|
+      hash[x] += 1
+    end
+
+    hash.each do |z, x|
+      puts "#{z} appears #{x} times"
+    end
   end  
 end
