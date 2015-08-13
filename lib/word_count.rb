cd3b2c4aaa348c7c6a188acaf437441c05c3e08e
Carson Trent Long <CarsonLong@IOS-Syncy.local>
2015-08-13 10:18:29 +0800

    finished unique_word_count, started word_frequency_count

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,7 +1,16 @@
 class String
   def unique_word_count
+    return 0 if self.length == 0
+    return self.gsub(/[^a-z ]/, '').scan(/\w+/).length
   end
   
   def word_frequency_count
+    return 0 if self.length == 0
+    array = self.scan(/\w+/)
+    init = array[0]
+    count = 1
+    array.length.times {
+      
+    }
   end  
 end
